ee2905d00c1f47dda0c468cae2b57d75ccfd11f8
mli <mli@ableton.com>
2015-11-13 09:11:38 +0100

    Revert "Measure: no path traversal"
    This reverts commit adab2e03eddf3eac724a4f6d6e66b5a390ef325b.

--- src/StyleSet.cpp
+++ src/StyleSet.cpp
@@ -413,7 +413,7 @@ StyleSetAttached::StyleSetAttached(QObject* pParent)
       }
     }
 
-    // mPath = traversePathUp(p);
+    mPath = traversePathUp(p);
 
     mStyleEngineLoadedConnection = QObject::connect(StyleEngineHost::globalStyleEngineHost(),
             &StyleEngineHost::styleEngineLoaded, [this] (aqt::stylesheets::StyleEngine* pStyleEngine)
@@ -470,7 +470,7 @@ void StyleSetAttached::setName(const QString& val)
 
     QObject* p = parent();
     if (p) {
-      // mPath = traversePathUp(p);
+      mPath = traversePathUp(p);
       setupStyle();
     }
 
@@ -500,7 +500,7 @@ void StyleSetAttached::onParentChanged(QQuickItem* pNewParent)
 {
   QObject* pParent = parent();
   if (pNewParent != nullptr && pParent != nullptr) {
-    // mPath = traversePathUp(pParent);
+    mPath = traversePathUp(pParent);
     setupStyle();
 
     Q_EMIT pathChanged(QString::fromStdString(pathToString(mPath)));
